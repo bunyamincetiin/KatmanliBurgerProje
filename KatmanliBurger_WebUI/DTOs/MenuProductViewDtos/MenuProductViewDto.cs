453bd5ed05fd16b66a370d97db22389ad80386d0
bunyamincetiin <bbunyamincetin@gmail.com>
2023-11-16 00:28:00 +0300

    create main page UI but not completed

---
+++ KatmanliBurger_WebUI/DTOs/MenuProductViewDtos/MenuProductViewDto.cs
@@ -0,0 +1,12 @@
+﻿using KatmanliBurger_DATA.Concretes;
+
+namespace KatmanliBurger_WebUI.DTOs.MenuProductViewDtos
+{
+    public class MenuProductViewDto
+    {
+        public IEnumerable<Menu> Menus { get; set; }
+        public IEnumerable<Burger> Burgers { get; set; }
+        public IEnumerable<ByProduct> ByProducts { get; set; }
+        public Category Category { get; set; }
+    }
+}
